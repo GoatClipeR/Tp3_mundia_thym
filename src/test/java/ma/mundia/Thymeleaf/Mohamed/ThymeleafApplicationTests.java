32ae3f74eed339fcd2e7b58bc1a8d8f62b34fc38
Mohamed <bhsnmohamed@gmail.com>
2025-11-26 01:14:48 +0100

    Initial commit

---
+++ src/test/java/ma/mundia/Thymeleaf/Mohamed/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.mundia.Thymeleaf.Mohamed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
